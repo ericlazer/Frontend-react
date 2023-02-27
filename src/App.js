7a844adb25e56cfe4d04f20ce428d3ccd9e17b96
Dark Night <darknight.dev0918@gmail.com>
2023-02-27 13:51:07 +0900

    Added Layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import SignIn from './pages/signin';
 import SignUp from './pages/signup';
 import Dashboard from './pages/dashboard';
+import Main from './pages/app';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         {/* <Route path = "/" element ={<Navigate to = "/signup"/>} /> */}
-        <Route path = "/" element = {<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/app" element={<Main />} />
         <Route path = "/signin" element = {<SignIn/>} />
         <Route path = "/signup" element = {<SignUp />} />
       </Routes>
---
+++ src/components/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Layout = ({children}) => {
+  return (
+    <div className='flex'>
+      <div className='w-[300px] bg-black min-h-screen p-4 flex flex-col text-xl text-white'>
+        <div className='cursor-pointer transition ease-in-out duration-300 hover:bg-gray-800 rounded-lg p-3'>Coins</div>
+        <div className='cursor-pointer transition ease-in-out duration-300 hover:bg-gray-800 rounded-lg p-3'>NFT</div>
+        <div className='cursor-pointer transition ease-in-out duration-300 hover:bg-gray-800 rounded-lg p-3'>Dapp</div>
+        <div className='cursor-pointer transition ease-in-out duration-300 hover:bg-gray-800 rounded-lg p-3'>DeFi</div>
+        <div className='cursor-pointer transition ease-in-out duration-300 hover:bg-gray-800 rounded-lg p-3'>News</div>
+        <div className='cursor-pointer transition ease-in-out duration-300 hover:bg-gray-800 rounded-lg p-3'>Insights</div>
+      </div>
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout;
