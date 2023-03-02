38136af6d1bad4db9d93c03ce14faadff8bf8d15
Dark Night <darknight.dev0918@gmail.com>
2023-03-02 15:40:35 +0900

    Added Router

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
             <Link><div className='text-[14px] tracking-[.05em] text-gray-300 hover:text-white transition ease-in-out hidden sm:block'>HOW-TO</div></Link>
             <Link><div className='text-[14px] tracking-[.05em] text-gray-300 hover:text-white transition ease-in-out hidden sm:block'>CAREER</div></Link>
             <Link><div className='text-[14px] tracking-[.05em] text-gray-300 hover:text-white transition ease-in-out hidden sm:block'>ABOUT</div></Link>
-            <Link to="/app"><div className='py-2 px-6 rounded-md px-8 duration-300 transition cursor-pointer ease-in-out text-[14px] hover:opacity-[0.8]' style={{background: 'radial-gradient(100% 341.37% at 0% 24.29%, #5C1693 0%, #201DAD 100%)'}}>Ged Started</div></Link>
+            <Link to="/coins"><div className='py-2 px-6 rounded-md px-8 duration-300 transition cursor-pointer ease-in-out text-[14px] hover:opacity-[0.8]' style={{background: 'radial-gradient(100% 341.37% at 0% 24.29%, #5C1693 0%, #201DAD 100%)'}}>Ged Started</div></Link>
           </div>
         </div>
         <div className='mt-32 text-[30px] sm:text-[40px] lg:text-[62px] text-center font-bold'>
@@ -26,10 +26,10 @@ const Dashboard = () => {
           With our technology and user-friendly platform, you can easily manage, store information and <br/> get recent news about Blockchain.
         </div>
         <div className='mt-8 flex justify-center gap-5'>
-          <Link to="/app">
+          <Link to="/coins">
             <button className='text-sm font-semibold text-black bg-white px-6 py-3 rounded-md cursor-pointer transition ease-in-out duration-300 hover:opacity-[0.8]' style={{background: 'radial-gradient(100% 341.37% at 0% 24.29%, #FFFFFF 0%, #C8C8C8 100%)'}} >GET STARTED</button>
           </Link>
-          <Link to="/app">
+          <Link to="/coins">
             <button className='text-sm text-white font-semibold px-6 py-3 rounded-md cursor-pointer transition ease-in-out duration-300 hover:opacity-[0.8]' style={{background: 'radial-gradient(100% 341.37% at 0% 24.29%, rgba(255, 255, 255, 0.17) 0%, rgba(200, 200, 200, 0.14) 100%)'}}>WATCH VIDEO</button>
           </Link>
         </div>
