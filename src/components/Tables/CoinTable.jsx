856a4441e8c76cc38d71dafe78b0b2fc2595b8de
Dark Night <darknight.dev0918@gmail.com>
2023-03-07 05:13:44 +0900

    Coin Table

--- src/components/Tables/CoinTable.jsx
+++ src/components/Tables/CoinTable.jsx
@@ -1,87 +1,116 @@
-import React, { useState } from "react";
-import { CoinTableHeader } from "../../config/TableHeaders";
+import React, { useState, useEffect } from "react";
 
-// map table headers to object keys
-const columnMap = {
-  "#": "id",
-  "1h %": "hourlyChange",
-  "24h %": "dailyChange",
-  "7d %": "weeklyChange",
+// Table header width
+const columnWidths = {
+  id: "50px",
+  name: "250px",
+  price: "200px",
+  hourlyChanged: "150px",
+  dailyChanged: "150px",
+  weeklyChanged: "150px",
+  marketCap: "200px",
+  volume: "200px",
+};
+
+// Table Headers
+const CoinTableHeader = [
+  { header: "#", columnName: "id" },
+  { header: "Name", columnName: "name" },
+  { header: "Price", columnName: "price" },
+  { header: "1h %", columnName: "hourlyChanged" },
+  { header: "24h %", columnName: "dailyChanged" },
+  { header: "7d %", columnName: "weeklyChanged" },
+  { header: "Market Cap", columnName: "marketCap" },
+  { header: "Volume (24h)", columnName: "volume" },
+];
+
+// Format number for biggest number
+const formatNumber = (num) => {
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(2) + "B";
+  } else if (num >= 1000000) {
+    return (num / 1000000).toFixed(2) + "M";
+  } else {
+    return num.toFixed(2);
+  }
+};
+
+const calculatePercentage = (value, max) => {
+  return (value / max) * 100;
 };
 
 const Table = (props) => {
-  const { CoinData } = props;
-
   const [sortBy, setSortBy] = useState("");
   const [sortAsc, setSortAsc] = useState(false);
-  const [tableData, setTableData] = useState(CoinData);
+  const [tableData, setTableData] = useState([]);
 
-  const sortByColumn = (column) => {
+  // Get maxium values for marketcap and volume
+  const maxMarketCap = tableData.reduce((max, data) => {
+    return Math.max(max, data.marketCap);
+  }, 0);
+  
+  const maxVolume = tableData.reduce((max, data) => {
+    return Math.max(max, data.volume);
+  }, 0);
 
-    const sortOrder = (sortBy === column ? !sortAsc : false) ? 1 : -1;
+  const sortTableData = (data) => {
+    if (!sortBy) {
+      return data;
+    }
 
-    // Extract the data to remove special characters. For e.g, '$', '%'
-    const preprocessedData = tableData.map((data) => {
-      if (
-        column === "hourlyChange" ||
-        column === "dailyChange" ||
-        column === "weeklyChange"
-      ) {
-        // Get the number from string.
-        var extractedNumber = data[column].replace("%", "");
-        return { ...data, [column]: extractedNumber };
-      } else {
-        return data;
-      }
-    });
+    const sortedData = [...data].sort((a, b) => {
+      const sortOrder = sortAsc ? -1 : 1;
 
-    // Sort the data by it's column
-    const sortedData = [...preprocessedData].sort((a, b) => {
-      if (
-        column === "id" ||
-        column === "price" ||
-        column === "hourlyChange" ||
-        column === "dailyChange" ||
-        column === "weeklyChange"
-      ) {
-        // In these columns, compare by integer. Not string
-        const valueA = parseFloat(a[column]);
-        const valueB = parseFloat(b[column]);
+      // If the table header is not name, sort by integer
+      if (sortBy !== "name") {
+        const valueA = parseFloat(a[sortBy]);
+        const valueB = parseFloat(b[sortBy]);
         if (valueA < valueB) return -sortOrder;
         if (valueA > valueB) return sortOrder;
         return 0;
       } else {
-        if (a[column] < b[column]) return -sortOrder;
-        if (a[column] > b[column]) return sortOrder;
+        if (a[sortBy] < b[sortBy]) return -sortOrder;
+        if (a[sortBy] > b[sortBy]) return sortOrder;
         return 0;
       }
     });
 
-    // Set the data to show in the table
-    setTableData(sortedData);
-    setSortBy(column);
-    setSortAsc(sortBy === column ? !sortAsc : false);
+    return sortedData;
   };
 
+  // Sort the table function
+  const handleSort = (column) => {
+    if (column === sortBy) {
+      setSortAsc(!sortAsc);
+    } else {
+      setSortBy(column);
+      setSortAsc(true);
+    }
+  };
+
+  useEffect(() => {
+    setTableData(sortTableData(props.CoinData));
+    console.log(props.CoinData);
+  }, [props.CoinData, sortBy, sortAsc]);
+
   return (
-    <table className="">
+    <table className="mx-auto">
       <thead>
         <tr className="text-white">
-          {CoinTableHeader.map((header, index) => {
-
-            const columnName = columnMap[header.toLowerCase()] || header.toLowerCase();
-            
+          {CoinTableHeader.map(({ header, columnName }, index) => {
             return (
               <th
                 key={index}
                 className={`${
                   header === "Name" ? "text-left" : "text-right"
-                } px-4 border-b border-slate-800 pb-6 cursor-pointer select-none text-[14px] w-[60px]`}
+                } px-4 border-b border-slate-800 pb-6 cursor-pointer select-none text-[14px] w-[${
+                  columnWidths[columnName]
+                }]`}
                 onClick={() => {
-                  sortByColumn(columnName);
+                  handleSort(columnName);
                 }}
               >
-                {sortBy === columnName && (sortAsc ? " ▲" : " ▼")} {header}
+                {sortBy === columnName && (sortAsc ? " ▼" : " ▲")} {header}
               </th>
             );
           })}
@@ -90,20 +119,46 @@ const Table = (props) => {
       <tbody>
         {tableData.map((data, index) => (
           <tr key={index} className="text-white border-b border-slate-800">
-            <td className="p-6 w-[50px] px-4 text-right">{data.id}</td>
-            <td className="w-[300px] px-4">{data.name}</td>
-            <td className="w-[100px] px-4 text-right">
+            <td className="p-6 text-right">{data.id}</td>
+            <td className="p-6 px-4 flex items-center">
+              <div>
+                <img src={data.imgURL} className="h-6 w-6" />
+              </div>
+              <div className="ml-4">
+                <div className="font-semibold">{data.name}</div>
+              </div>
+            </td>
+            <td className="px-4 text-right">
               {parseFloat(data.price).toLocaleString("en-US", {
                 style: "currency",
                 currency: "USD",
               })}
             </td>
-            <td className="w-[100px] px-4 text-right">{data.hourlyChange}%</td>
-            <td className="w-[100px] px-4 text-right">{data.dailyChange}%</td>
-            <td className="w-[100px] px-4 text-right">{data.weeklyChange}%</td>
-            <td className="w-[200px] px-4 text-right">{data.marketCap}</td>
-            <td className="w-[200px] px-4 text-right">{data.volume}</td>
-            <td className="w-[200px] px-4 text-right">{data.lastSevenDays}</td>
+            <td className="px-4 text-right">
+              {data.hourlyChanged.toFixed(2)}%
+            </td>
+            <td className="px-4 text-right">{data.dailyChanged.toFixed(2)}%</td>
+            <td className="px-4 text-right">
+              {data.weeklyChanged.toFixed(2)}%
+            </td>
+            <td className="px-4 text-right">
+              ${formatNumber(data.marketCap)}
+              <div className="h-2 bg-gray-300 mt-2">
+                <div
+                  className="h-full bg-green-500"
+                  style={{ width: `${calculatePercentage(data.marketCap, maxMarketCap)}%` }}
+                ></div>
+              </div>
+            </td>
+            <td className="px-4 text-right">
+              ${formatNumber(data.volume)}
+              <div className="h-2 bg-gray-300 mt-2">
+                <div
+                  className="h-full bg-blue-500"
+                  style={{ width: `${calculatePercentage(data.volume, maxVolume)}%` }}
+                ></div>
+              </div>
+            </td>
           </tr>
         ))}
       </tbody>
--- src/pages/coins/index.jsx
+++ src/pages/coins/index.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import socketIOClient from 'socket.io-client'
 import Layout from '../../components/Layout'
 import CoinTable from '../../components/Tables/CoinTable'
 import CoinMarketImg from '../../assets/img/CoinMarket.gif'
+import { API_URL } from '../../config/constants'
 
 const Coins = () => {
 
-  const CoinData = [
-    { id: 1, name: 'Bitcoin', price: '45632', hourlyChange: '-0.76', dailyChange: '-2.91', weeklyChange: '-1.12', marketCap: '$858.8B', volume: '$36.5B', lastSevenDays: 0.5 },
-    { id: 2, name: 'Ethereum', price: '1470', hourlyChange: '-1.28', dailyChange: '-4.22', weeklyChange: '-1.36', marketCap: '$168.3B', volume: '$22.3B', lastSevenDays: 0.5 },
-    { id: 3, name: 'Cardano', price: '121', hourlyChange: '+0.11', dailyChange: '+2.58', weeklyChange: '+25.25', marketCap: '$38.7B', volume: '$2.7B', lastSevenDays: 0.5 },
-    { id: 4, name: 'Binance Coin', price: '221', hourlyChange: '-0.94', dailyChange: '-1.89', weeklyChange: '+29.63', marketCap: '$34.1B', volume: '$2.2B', lastSevenDays: 0.5 },
-    { id: 5, name: 'Tether', price: '1', hourlyChange: '+0.06', dailyChange: '+0.06', weeklyChange: '+0.06', marketCap: '$33.6B', volume: '$120.9B', lastSevenDays: 0.5 }
-  ]
+  const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    const socket = socketIOClient(API_URL);
+
+    // Get Total Coin Data
+    socket.on('totalCoinInfo', data => {
+      setCoinData(data);
+    })
+  }, [])
 
   return (
     <Layout>
@@ -46,7 +51,7 @@ const Coins = () => {
         <div className='mt-16'>
           <p className='text-center text-[40px] font-bold text-white'>Crypto Prices Today</p>
           <div className='m-8'>
-            <CoinTable CoinData={CoinData} />
+            <CoinTable CoinData={coinData} />
           </div>
         </div>
       </div>
