cbdb26a28ccef5ab41860a6d56b9fc392971ac76
Dark Night <darknight.dev0918@gmail.com>
2023-03-07 09:31:28 +0900

    Change Port

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 const DEV_MODE = true;
-export const API_BASE = DEV_MODE === true?'http://localhost:8000/api/v1':'';
-export const API_URL = DEV_MODE === true ? 'http://localhost:8000/' : 'http://103.179.142.70:8000/';
+export const API_BASE = DEV_MODE === true?'http://localhost:5000/api/v1':'';
+export const API_URL = DEV_MODE === true ? 'http://localhost:5000/' : '';
