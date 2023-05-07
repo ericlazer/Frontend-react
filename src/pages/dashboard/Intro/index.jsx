b85a85f241a28b80283e0cbdc3b90c8c6d667e23
Dark Night <darknight.dev0918@gmail.com>
2023-05-07 16:50:42 +0900

    Make the Fonts bigger

--- src/pages/dashboard/Intro/index.jsx
+++ src/pages/dashboard/Intro/index.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <>
       <div className='flex justify-between'>
-        <p className='text-[65px] text-white font-[DotBold]'>daisugi</p>
+        <p className='text-[60px] md:text-[75px] text-white font-[DotBold]'>daisugi</p>
         {/* <div className='flex text-xl gap-0 sm:gap-10 text-white items-center'>
           <Link><div className='text-[14px] tracking-[.05em] text-gray-300 hover:text-white transition ease-in-out hidden sm:block'>HOW-TO</div></Link>
           <Link><div className='text-[14px] tracking-[.05em] text-gray-300 hover:text-white transition ease-in-out hidden sm:block'>CAREER</div></Link>
@@ -15,9 +15,9 @@ const Intro = () => {
       </div>
       <div className='mt-8 justify-center items-center flex flex-col gap-5'>
         <div>
-          <img src="/img/LandingPage_tree.png" className='rounded-lg' width={600} alt="Landing Page Tree" />
+          <img src="/img/LandingPage_tree.png" className='rounded-lg' width={800} alt="Landing Page Tree" />
         </div>
-        <div className='text-md text-white text-center -mt-[50px] md:text-2xl'>
+        <div className='text-2xl text-white text-center -mt-[50px] md:-mt-[100px] md:text-4xl font-[DotBold]'>
           Gain a competitive edge with Daisugi - the ultimate cross-chain trading terminal.
         </div>
         <div>
